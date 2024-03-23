e4a00a1e94b53309681ed2b0faad9957be176834
Gabriel Gutierrez Semidey <56008696+gabrieljesus11@users.noreply.github.com>
2024-03-23 16:14:26 -0300

    Initial Springboot Scaffolding

---
+++ src/main/java/ar/edu/uade/moviePlay/MoviePlayApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.uade.moviePlay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviePlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/uade/moviePlay/MoviePlayApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.uade.moviePlay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviePlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
