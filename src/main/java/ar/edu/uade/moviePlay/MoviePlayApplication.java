41c08c8906d4b172e4e5ad732bd6aab8937d41b8
gabrieljesus11 <gabrieljesus11@gmail.com>
2024-06-13 11:48:35 -0300

    DatabaseConfiguration

--- src/main/java/ar/edu/uade/moviePlay/MoviePlayApplication.java
+++ src/main/java/ar/edu/uade/moviePlay/MoviePlayApplication.java
@@ -2,9 +2,15 @@ package ar.edu.uade.moviePlay;
 
 import ar.edu.uade.moviePlay.util.ScopeUtils;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.*")
+@ComponentScan(basePackages = { "com.*" })
+@EntityScan("my.package.base.*")
 public class MoviePlayApplication {
 
 	public static void main(String[] args) {
