4a9ac407debb63c2d22e835217ea73e8a569bc0d
gabrieljesus11 <gabrieljesus11@gmail.com>
2024-06-13 10:38:19 -0300

    Crud Repository

--- src/main/java/ar/edu/uade/moviePlay/repository/IUserRepository.java
+++ src/main/java/ar/edu/uade/moviePlay/repository/IUserRepository.java
@@ -2,16 +2,16 @@ package ar.edu.uade.moviePlay.repository;
 
 import ar.edu.uade.moviePlay.entity.User;
 import jakarta.transaction.Transactional;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface IUserRepository extends JpaRepository<User, Integer> {
+public interface IUserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByEmail(String email);
     @Transactional
     @Modifying
