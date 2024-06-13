d1bf512580801139211a66c012b0c0fa193d8bed
gabrieljesus11 <gabrieljesus11@gmail.com>
2024-06-13 10:09:54 -0300

    JPA Repos

--- src/main/java/ar/edu/uade/moviePlay/MoviePlayApplication.java
+++ src/main/java/ar/edu/uade/moviePlay/MoviePlayApplication.java
@@ -3,8 +3,10 @@ package ar.edu.uade.moviePlay;
 import ar.edu.uade.moviePlay.util.ScopeUtils;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class MoviePlayApplication {
 
 	public static void main(String[] args) {
