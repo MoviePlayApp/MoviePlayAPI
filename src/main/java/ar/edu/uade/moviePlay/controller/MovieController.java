e7beee886478ec75c84f60e74677802edf3a16d6
unknown <solmuscara@gmail.com>
2024-07-18 17:22:22 -0300

    rating

--- src/main/java/ar/edu/uade/moviePlay/controller/MovieController.java
+++ src/main/java/ar/edu/uade/moviePlay/controller/MovieController.java
@@ -1,13 +1,15 @@
 package ar.edu.uade.moviePlay.controller;
 
 import ar.edu.uade.moviePlay.dto.movie.MovieDataDTO;
+import ar.edu.uade.moviePlay.dto.movie.MovieRateValueRequestDTO;
+import ar.edu.uade.moviePlay.dto.movie.MovieRateValueResponseDTO;
 import ar.edu.uade.moviePlay.service.Impl.MovieDetailsServiceImpl;
 import ar.edu.uade.moviePlay.service.Impl.MovieServiceImpl;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import ar.edu.uade.moviePlay.service.Impl.RatingServiceImpl;
+import ar.edu.uade.moviePlay.service.RatingService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import ar.edu.uade.moviePlay.dto.movie.GetMovieDTO;
 import ar.edu.uade.moviePlay.service.MovieService;
@@ -18,10 +20,12 @@ public class MovieController {
 
     private final MovieService movieService;
     private final MovieDetailsService movieDetailsService;
+    private final RatingService ratingService;
 
-    public MovieController(MovieServiceImpl movieService, MovieDetailsServiceImpl movieDetailsService){
+    public MovieController(MovieServiceImpl movieService, MovieDetailsServiceImpl movieDetailsService, RatingServiceImpl ratingService){
         this.movieService = movieService;
         this.movieDetailsService = movieDetailsService;
+        this.ratingService = ratingService;
     }
 
     @GetMapping("/movies")
@@ -41,4 +45,18 @@ public class MovieController {
             @PathVariable String movieId) {
         return movieDetailsService.getMovieDetails(movieId);
     }
+
+    @PutMapping("/movies/{movieId}/rating")
+    @ResponseStatus(HttpStatus.OK)
+    public MovieRateValueResponseDTO updateMovieRating(
+            @PathVariable String movieId, @RequestBody MovieRateValueRequestDTO request) {
+        String updateStatus = ratingService.updateMovieRating(movieId, request.getRateValue());
+        MovieDataDTO updatedMovieDetails = movieDetailsService.getMovieDetails(movieId);
+        return MovieRateValueResponseDTO.builder()
+                .movieId(Integer.parseInt(movieId))
+                .movieRate(updatedMovieDetails.getVote_average())
+                .statusMessage(updateStatus)
+                .build();
+    }
+
 }
